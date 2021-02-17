73c0083986ed08807f786f1cfd689339534a0b46
Philipp M. Wolf <philipp.wolf@physics.uu.se>
2021-02-17 19:28:43 +0100

    Worked on day 2

---
+++ Day_2_exersise/matmult.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+# Program to multiply two matrices using nested loops
+import random
+#import line_profiler
+#profile = line_profiler.LineProfiler()
+
+
+N = 250
+
+# NxN matrix
+@profile
+def MultiplyMatrix(N):
+    X = []
+    
+    
+    for i in range(N):
+        X.append([random.randint(0,100) for r in range(N)])
+        
+    
+    # Nx(N+1) matrix
+    Y = []
+    
+    for i in range(N):
+        Y.append([random.randint(0,100) for r in range(N+1)])
+        
+    
+    # result is Nx(N+1)
+    result = []
+    
+    for i in range(N):
+        result.append([0] * (N+1))
+        print(result)
+    
+    # iterate through rows of X
+    
+    for i in range(len(X)):
+        # iterate through columns of Y
+        for j in range(len(Y[0])):
+            # iterate through rows of Y
+            for k in range(len(Y)):
+                result[i][j] += X[i][k] * Y[k][j]
+    
+    for r in result:
+        print(r)
+    return result
+
+x = MultiplyMatrix(N)
